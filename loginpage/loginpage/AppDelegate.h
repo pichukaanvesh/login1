227013d4d11220e164912c12e74112b3195af0f7
Public Users <pichukaanvesh@gmail.com>
2017-07-11 12:04:07 +0530

    project template

---
+++ loginpage/loginpage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  loginpage
+//
+//  Created by Public Users on 11/07/2017.
+//  Copyright © 2017 Public Users. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ loginpage/loginpage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  loginpage
+//
+//  Created by Public Users on 11/07/2017.
+//  Copyright © 2017 Public Users. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
